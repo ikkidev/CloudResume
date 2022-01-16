65975d2aeaa307a0f325ea543fb5a08c960fee93
Ikki <4778641+ikkidev@users.noreply.github.com>
2022-01-16 17:01:42 -0500

    Set version requirements for terraform providers

---
+++ tf/providers.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+
+//ACM certificate with Amazon Cloudfront is only supported on us-east-1
+//https://docs.aws.amazon.com/acm/latest/userguide/acm-regions.html
+provider "aws" {
+  alias = "aws_cloudfront_provider"
+  region = "us-east-1"
+}
