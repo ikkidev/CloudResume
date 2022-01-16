b249c1c474ce2a262ef67a4dc7c707ca8b1c3268
Ikki <4778641+ikkidev@users.noreply.github.com>
2022-01-16 17:19:11 -0500

    Add config to create s3 bucket

---
+++ tf/env/dev.tfvars
@@ -0,0 +1,6 @@
+domain_name = "www.ikkidev.com"
+bucket_name = "ikkidev.com"
+
+common_tags = {
+  Project = "ikki-cloud-resume"
+}
---
+++ tf/variables.tf
@@ -0,0 +1,13 @@
+variable "domain_name" {
+  type = string
+  description = "The domain name for the website"
+}
+
+variable "common_tags" {
+  description = "The common tag to apply to all components"
+}
+
+variable "bucket_name" {
+  type = string
+  description = "The globally unique name for S3 bucket.Typically set to domain_name without the www prefix"
+}
