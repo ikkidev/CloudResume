e31b857f0a233c7070d72e916fddc1bec685fdc5
Ikki <4778641+ikkidev@users.noreply.github.com>
2022-10-08 16:13:03 -0400

    Fixed github action workflow errors

--- terraform-cloud-resume/modules/apigateway/main.tf
+++ terraform-cloud-resume/modules/apigateway/main.tf
@@ -1,6 +1,9 @@
 resource "aws_apigatewayv2_api" "apigw" {
   name          = "serverless_lambda_gw"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["*"]
+  }
 }
 
 resource "aws_apigatewayv2_stage" "lambda" {
